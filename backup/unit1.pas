5e73efb515ba08092bb1779c175962ea22f70d80
Carlos Brito <carlosbrito202006@gmail.com>
2026-03-04 19:13:04 -0400

    Le coloqué la función abrir

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -13,25 +13,34 @@ type
   TForm1 = class(TForm)
     EditRuta: TEdit;
     ImageList1: TImageList;
-    MenuItem1: TMenuItem;
-    MenuItem2: TMenuItem;
+    MenuItem1: TMenuItem; // Renombrar
+    MenuItem2: TMenuItem; // Borrar
+    MenuItem3: TMenuItem; // Abrir
     PopupMenu1: TPopupMenu;
     ShellListView1: TShellListView;
     ShellTreeView1: TShellTreeView;
     StatusBar1: TStatusBar;
     ToolBar1: TToolBar;
+    btnAtras: TToolButton;
+    btnAdelante: TToolButton;
+    procedure btnAdelanteClick(Sender: TObject);
+    procedure btnAtrasClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure EditRutaKeyDown(Sender: TObject; var Key: Word; {%H-}Shift: TShiftState);
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
-    procedure ShellListView1Change(Sender: TObject; Item: TListItem;
-      Change: TItemChange);
+    procedure MenuItem3Click(Sender: TObject);
+    procedure ShellListView1Change(Sender: TObject; Item: TListItem; Change: TItemChange);
     procedure ShellListView1Click(Sender: TObject);
     procedure ShellListView1DblClick(Sender: TObject);
     procedure ShellTreeView1Click(Sender: TObject);
     procedure ShellTreeView1GetImageIndex(Sender: TObject; Node: TTreeNode);
   private
+    ListaAtras: TStringList;
+    ListaAdelante: TStringList;
     procedure ActualizarEstado;
+    procedure Navegar(const NuevaRuta: string; AddToHistory: Boolean);
   public
   end;
 
@@ -46,21 +55,79 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  ShellTreeView1.Root := '';  // <- comillas vacías = mostrar todos los discos
+  ListaAtras := TStringList.Create;
+  ListaAdelante := TStringList.Create;
+
+  ShellTreeView1.Root := '';
   ShellListView1.ViewStyle := vsReport;
-  ShellListView1.Refresh;
-  ShellListView1.ImageIndex := 0;
+  ActualizarEstado;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  // Liberamos la memoria de las listas al cerrar el programa
+  ListaAtras.Free;
+  ListaAdelante.Free;
+end;
+
+procedure TForm1.Navegar(const NuevaRuta: string; AddToHistory: Boolean);
+begin
+  if (NuevaRuta = '') or (not DirectoryExists(NuevaRuta)) then Exit;
+
+  if AddToHistory then
+  begin
+    // Solo guardamos en el historial si la ruta es distinta a la actual
+    if ShellListView1.Root <> NuevaRuta then
+    begin
+      ListaAtras.Add(ShellListView1.Root);
+      ListaAdelante.Clear; // Nueva navegación limpia el "adelante"
+    end;
+  end;
+
+  ShellListView1.Root := NuevaRuta;
+  ShellTreeView1.Path := NuevaRuta;
+  EditRuta.Text := NuevaRuta;
+  ActualizarEstado;
+end;
+
+procedure TForm1.btnAtrasClick(Sender: TObject);
+var
+  RutaAnterior: string;
+begin
+  if ListaAtras.Count > 0 then
+  begin
+    RutaAnterior := ListaAtras[ListaAtras.Count - 1];
+    ListaAtras.Delete(ListaAtras.Count - 1);
+
+    // Antes de ir atrás, guardamos la actual en adelante
+    ListaAdelante.Add(ShellListView1.Root);
+
+    Navegar(RutaAnterior, False);
+  end;
+end;
+
+procedure TForm1.btnAdelanteClick(Sender: TObject);
+var
+  RutaSiguiente: string;
+begin
+  if ListaAdelante.Count > 0 then
+  begin
+    RutaSiguiente := ListaAdelante[ListaAdelante.Count - 1];
+    ListaAdelante.Delete(ListaAdelante.Count - 1);
+
+    // Antes de ir adelante, guardamos la actual en atrás
+    ListaAtras.Add(ShellListView1.Root);
+
+    Navegar(RutaSiguiente, False);
+  end;
 end;
 
 procedure TForm1.EditRutaKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
 begin
-  if Key = VK_RETURN then // Si el usuario presiona Enter
+  if Key = VK_RETURN then
   begin
     if DirectoryExists(EditRuta.Text) then
-    begin
-      ShellListView1.Root := EditRuta.Text;
-      ShellTreeView1.Path := EditRuta.Text;
-    end
+      Navegar(EditRuta.Text, True)
     else
       ShowMessage('La ruta no existe.');
   end;
@@ -68,90 +135,68 @@ end;
 
 procedure TForm1.ActualizarEstado;
 begin
-  // Verificamos que los componentes existan antes de usarlos
-  if Assigned(EditRuta) then
-    EditRuta.Text := ShellTreeView1.Path;
-
   if Assigned(StatusBar1) then
     StatusBar1.SimpleText := 'Elementos: ' + IntToStr(ShellListView1.Items.Count);
+
+  // Habilitar o deshabilitar botones según el historial
+  btnAtras.Enabled := ListaAtras.Count > 0;
+  btnAdelante.Enabled := ListaAdelante.Count > 0;
 end;
 
 procedure TForm1.ShellTreeView1Click(Sender: TObject);
 begin
-  ActualizarEstado;
+  // Cuando clicamos en el árbol, navegamos y actualizamos historial
+  if DirectoryExists(ShellTreeView1.Path) then
+    Navegar(ShellTreeView1.Path, True);
 end;
 
 procedure TForm1.ShellTreeView1GetImageIndex(Sender: TObject; Node: TTreeNode);
 begin
-  // Verificamos si el nodo actual es un directorio
   if DirectoryExists(ShellTreeView1.GetPathFromNode(Node)) then
-    Node.ImageIndex := 0  // Índice de la carpeta en tu ImageList
+    Node.ImageIndex := 0
   else
-    Node.ImageIndex := 1; // Índice del documento en tu ImageList
-
-  // Esto asegura que el icono se mantenga igual cuando se selecciona
+    Node.ImageIndex := 1;
   Node.SelectedIndex := Node.ImageIndex;
 end;
 
 procedure TForm1.ShellListView1DblClick(Sender: TObject);
 var
-  RutaSeleccionada: string;
+  RutaSel: string;
 begin
   if Assigned(ShellListView1.Selected) then
   begin
-    RutaSeleccionada := ShellListView1.GetPathFromItem(ShellListView1.Selected);
-
-    if DirectoryExists(RutaSeleccionada) then
-    begin
-      // NAVEGACIÓN: Entrar a la carpeta
-      ShellListView1.Root := RutaSeleccionada;
-      ShellTreeView1.Path := RutaSeleccionada;
-      if Assigned(EditRuta) then EditRuta.Text := RutaSeleccionada;
-    end
+    RutaSel := ShellListView1.GetPathFromItem(ShellListView1.Selected);
+    if DirectoryExists(RutaSel) then
+      Navegar(RutaSel, True)
     else
-      OpenDocument(RutaSeleccionada); // ABRIR: Si es archivo
+      OpenDocument(RutaSel);
   end;
 end;
 
+procedure TForm1.MenuItem3Click(Sender: TObject);
+begin
+  // Reutilizamos el código del doble click para el menú "Abrir"
+  ShellListView1DblClick(Sender);
+end;
+
 procedure TForm1.MenuItem2Click(Sender: TObject);
-var
-  Ruta: string;
+var Ruta: string;
 begin
   if Assigned(ShellListView1.Selected) then
   begin
     Ruta := ShellListView1.GetPathFromItem(ShellListView1.Selected);
-    if QuestionDlg('Borrar', '¿Eliminar ' + ExtractFileName(Ruta) + '?',
-       mtConfirmation, [mrYes, mrNo], 0) = mrYes then
+    if QuestionDlg('Borrar', '¿Eliminar ' + ExtractFileName(Ruta) + '?', mtConfirmation, [mrYes, mrNo], 0) = mrYes then
     begin
       if DirectoryExists(Ruta) then
       begin
-        // Borra carpeta aunque tenga contenido
-        if not DeleteDirectory(Ruta, True) then
-          ShowMessage('No se pudo eliminar la carpeta.')
-        else
-          ShellListView1.Refresh;
+        if DeleteDirectory(Ruta, True) then ShellListView1.Refresh;
       end
       else
       begin
-        if not DeleteFile(Ruta) then
-          ShowMessage('No se pudo eliminar el archivo.')
-        else
-          ShellListView1.Refresh;
+        if DeleteFile(Ruta) then ShellListView1.Refresh;
       end;
     end;
   end;
-end;
-
-procedure TForm1.ShellListView1Change(Sender: TObject; Item: TListItem;
-  Change: TItemChange);
-begin
-
-end;
-
-procedure TForm1.ShellListView1Click(Sender: TObject);
-begin
-  if Assigned(ShellListView1.Selected) and Assigned(StatusBar1) then
-    StatusBar1.SimpleText := 'Seleccionado: ' + ShellListView1.Selected.Caption;
 end;
 
 procedure TForm1.MenuItem1Click(Sender: TObject);
@@ -169,17 +214,15 @@ begin
   end;
 end;
 
-//procedure TForm1.MenuItem2Click(Sender: TObject);
-//var Ruta: string;
-//begin
-  //if Assigned(ShellListView1.Selected) then
-  //begin
-    //Ruta := ShellListView1.GetPathFromItem(ShellListView1.Selected);
-    //if QuestionDlg('Borrar', '¿Eliminar ' + ExtractFileName(Ruta) + '?', mtConfirmation, [mrYes, mrNo], 0) = mrYes then
-    //begin
-      //if DeleteFile(Ruta) or RemoveDir(Ruta) then ShellListView1.Refresh;
-    //end;
-  //end;
-  //end;
+procedure TForm1.ShellListView1Click(Sender: TObject);
+begin
+  if Assigned(ShellListView1.Selected) and Assigned(StatusBar1) then
+    StatusBar1.SimpleText := 'Seleccionado: ' + ShellListView1.Selected.Caption;
+end;
+
+procedure TForm1.ShellListView1Change(Sender: TObject; Item: TListItem; Change: TItemChange);
+begin
+  // Opcional: Actualizar estado al cambiar selección
+end;
 
 end.
--- unit1.pas
+++ unit1.pas
@@ -15,21 +15,27 @@ type
     ImageList1: TImageList;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
     PopupMenu1: TPopupMenu;
     ShellListView1: TShellListView;
     ShellTreeView1: TShellTreeView;
     StatusBar1: TStatusBar;
     ToolBar1: TToolBar;
+    btnAtras: TToolButton;
+    btnAdelante: TToolButton;
+    procedure btnAtrasClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure EditRutaKeyDown(Sender: TObject; var Key: Word; {%H-}Shift: TShiftState);
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
     procedure ShellListView1Change(Sender: TObject; Item: TListItem;
       Change: TItemChange);
     procedure ShellListView1Click(Sender: TObject);
     procedure ShellListView1DblClick(Sender: TObject);
     procedure ShellTreeView1Click(Sender: TObject);
     procedure ShellTreeView1GetImageIndex(Sender: TObject; Node: TTreeNode);
+    procedure ToolBar1Click(Sender: TObject);
   private
     procedure ActualizarEstado;
   public
@@ -46,14 +52,19 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  ShellTreeView1.Root := '';  // <- comillas vacías = mostrar todos los discos
+  ShellTreeView1.Root := '';
   ShellListView1.ViewStyle := vsReport;
   ShellListView1.Refresh;
 end;
 
+procedure TForm1.btnAtrasClick(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.EditRutaKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
 begin
-  if Key = VK_RETURN then // Si el usuario presiona Enter
+  if Key = VK_RETURN then
   begin
     if DirectoryExists(EditRuta.Text) then
     begin
@@ -90,6 +101,11 @@ begin
 
   // Esto asegura que el icono se mantenga igual cuando se selecciona
   Node.SelectedIndex := Node.ImageIndex;
+end;
+
+procedure TForm1.ToolBar1Click(Sender: TObject);
+begin
+
 end;
 
 procedure TForm1.ShellListView1DblClick(Sender: TObject);
@@ -141,6 +157,31 @@ begin
   end;
 end;
 
+procedure TForm1.MenuItem3Click(Sender: TObject);
+var
+  RutaSeleccionada: string;
+begin
+  // Verificamos que haya algo seleccionado
+  if Assigned(ShellListView1.Selected) then
+  begin
+    RutaSeleccionada := ShellListView1.GetPathFromItem(ShellListView1.Selected);
+
+    if DirectoryExists(RutaSeleccionada) then
+    begin
+      // Si es CARPETA: Navegamos dentro de ella
+      ShellListView1.Root := RutaSeleccionada;
+      ShellTreeView1.Path := RutaSeleccionada;
+      if Assigned(EditRuta) then EditRuta.Text := RutaSeleccionada;
+      ActualizarEstado;
+    end
+    else if FileExists(RutaSeleccionada) then
+    begin
+      // Si es ARCHIVO: Lo abrimos con el programa predeterminado del sistema
+      OpenDocument(RutaSeleccionada);
+    end;
+  end;
+end;
+
 procedure TForm1.ShellListView1Change(Sender: TObject; Item: TListItem;
   Change: TItemChange);
 begin
@@ -167,18 +208,5 @@ begin
     end;
   end;
 end;
-
-//procedure TForm1.MenuItem2Click(Sender: TObject);
-//var Ruta: string;
-//begin
-  //if Assigned(ShellListView1.Selected) then
-  //begin
-    //Ruta := ShellListView1.GetPathFromItem(ShellListView1.Selected);
-    //if QuestionDlg('Borrar', '¿Eliminar ' + ExtractFileName(Ruta) + '?', mtConfirmation, [mrYes, mrNo], 0) = mrYes then
-    //begin
-      //if DeleteFile(Ruta) or RemoveDir(Ruta) then ShellListView1.Refresh;
-    //end;
-  //end;
-  //end;
 
 end.
